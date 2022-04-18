9af6385de020a4491749f11720faf7a27ff9eb76
rsensenig <r.a.sensenig@gmail.com>
2022-04-17 22:00:34 -0400

    updated admin and book routes, and hyperlinked edit button on admin-console

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -6,14 +6,11 @@ const adminCtrl = require('../controllers/admin-controller');
 // ROUTES
 router.route('/')
     .get(adminCtrl.admin)
-    .delete(adminCtrl.book_delete)
 
 router.route('/create-book')
     .get(adminCtrl.create)
-    .post(adminCtrl.book_create_post)
 
 router.route('/update-book/:_id')
     .get(adminCtrl.update)
-    .put(adminCtrl.book_update_put)
 
 module.exports = router;
--- routes/book-routes.js
+++ routes/book-routes.js
@@ -4,7 +4,12 @@ const router = express.Router();
 const bookCtrl = require('../controllers/book-controller');
 
 // ROUTES
+router.route('/')
+    .post(bookCtrl.book_create_post)
+
 router.route('/:_id')
     .get(bookCtrl.book)
+    .put(bookCtrl.book_update_put)
+    .delete(bookCtrl.book_delete)
 
 module.exports = router;
